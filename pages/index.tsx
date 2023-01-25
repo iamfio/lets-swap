bcb7278aba75887fdd3cb61c4b51c6c1ba0d1ef4
Fiodor Gorobet <iamfiogo@gmail.com>
2023-01-25 15:15:17 +0100

    first commit. nextjs, tailwind, prisma-mongodb

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Let's Swap!</title>
+        <meta name="description" content="Things swap community" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="text-3xl">Let's Swap!</h1>
+        <p>
+          It's a perfect place to exchange things you're not using right now.
+          Right here.
+        </p>
+      </main>
+    </>
+  )
+}
